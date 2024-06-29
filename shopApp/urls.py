8aef08c31379aaf4e319767e414ea320d8030685
Daniel Escobar <daanielescobar@gmail.com>
2024-06-28 23:21:46 -0600

    login and register

--- shopApp/urls.py
+++ shopApp/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("product/<int:product_id>", views.product, name="product")
+    path("product/<int:product_id>", views.product, name="product"),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('login/', views.login_user, name='login')
 ]
--- shopApp/views.py
+++ shopApp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
+from django.contrib.auth import authenticate, login
 from .models import Productos
+from .forms import UserRegistrationForm, LoginForm
 # Create your views here.
 
 def index(request):
@@ -13,3 +15,39 @@ def product(request, product_id):
     except Productos.DoesNotExist:
         raise Http404("Product does not exist")
     return render(request, "product.html", {"product": get_product})
+
+def profile(request):
+    return HttpResponse(request.session.session_key)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+            
+            #Log the user in and redirect to homepage
+            user = authenticate(username=new_user.username, password=form.cleaned_data['password'])
+            login(request, user)
+            return redirect('index') 
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'register.html', { 'form' : form})
+
+def login_user(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('profile')
+            else:
+                print('ola')
+                
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
